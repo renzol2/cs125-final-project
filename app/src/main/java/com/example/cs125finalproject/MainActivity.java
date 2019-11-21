2f1cda774af0ffa524d324b5b773e09050460397
Renzo Ledesma <renzol2@illinois.edu>
2019-11-20 19:15:16 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cs125finalproject/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.cs125finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * The main screen of the app. Made by Renzo and Saurav :)
+ *
+ * Basic idea so far: Tweet Unscrambling Game
+ *
+ * The user is presented with any random tweet, except its words have been scrambled up. The user
+ * has to put the tweet back together in its original form in as little time as possible.
+ *
+ * Points can be awarded based on completion accuracy or time of completion.
+ *
+ * Things to do:
+ * - find out how to use Twitter API lol
+ * - find out how to make a UI
+ * - basically everything...
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
